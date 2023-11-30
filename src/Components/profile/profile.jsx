0fa6199005d1a67f30e45975ce9052be6801e1a8
ragrelo <70542192+ragrelo@users.noreply.github.com>
2023-11-30 18:21:41 -0300

    Update profile.jsx

--- src/Components/profile/profile.jsx
+++ src/Components/profile/profile.jsx
@@ -3,8 +3,6 @@ import "./profile.css";
 import Nav from "../homeScreen/nav";
 import { useSelector, useDispatch } from "react-redux";
 import { auth } from "../../firebase";
-//import Plans from "./plans";
-//import Plans from "../homeScreen/homeScreen";
 
 function Profile() {
 	const { user } = useSelector((state) => state.user);
@@ -13,7 +11,7 @@ function Profile() {
 		<div className="profile">
 			<Nav />
 			<div className="profile__body">
-				<h1>Edit Profile</h1>
+				<h1>Editar Perfil</h1>
 				<div className="profile__info">
 					<img
 						src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
@@ -22,7 +20,7 @@ function Profile() {
 					<div className="profile__details">
 						<h2>{user.email}</h2>
 						<div className="profile__plans">
-							<h3>Plans</h3>
+							<h3>Planes</h3>
 							<Plans />
 							<button
 								onClick={() => auth.signOut()}
