348b8e93dcf6f441bb88fbafecbb38ed9ad35622
ragrelo <70542192+ragrelo@users.noreply.github.com>
2023-11-30 18:16:10 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import React, { useState, useRef }  from 'react'
 import './App.css'
-//import Row from './Components/Row/Row'
-//import requests from './request'
 import HomeScreen from "./components/homeScreen/homeScreen";
-//import Banner from './Components/Banner/Banner'
-//import Navbar from './Components/Navbar/Navbar'
 import Login from "./components/registration/login";
-//import Profile from "./components/profile/profile";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-	//const { user } = useSelector((state) => state.user);
-	//const dispatch = useDispatch();
   const user = true;
 
    return (
